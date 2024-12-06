d05c0039a5198e620dbe0d5c0c14f2953bd2595c
dricondop <dricondop@gmail.com>
2024-12-06 17:16:47 +0100

    slow intro text display also removed PrintWelcome now just PrintIntro

--- WorldOfZuul/WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
+++ WorldOfZuul/WorldOfZuul/obj/Debug/net6.0/WorldOfZuul.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80c94f195e0c159a59e13ad94c90867e8c253857")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+043639816b86e6228be332224bd9336522b27906")]
 [assembly: System.Reflection.AssemblyProductAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WorldOfZuul")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
